4a7371f972ecde22fe4f1d3eb330097d4be35768
Lim Jing <limjing7@gmail.com>
2019-04-17 14:03:49 -0700

    caches calculation

--- player/MiniMaxPlayer.java
+++ player/MiniMaxPlayer.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import org.ggp.base.apps.player.Player;
@@ -12,8 +13,16 @@ import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 
 public class MiniMaxPlayer extends ExampleLegalPlayer {
 
+    HashMap<MachineState, Integer> memo_table;
+
     public static void main(String[] args) {
         Player.initialize(new MiniMaxPlayer().getName());
+    }
+
+    @Override
+    public void start(long timeout)
+            throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+        this.memo_table = new HashMap<>();
     }
 
     @Override
@@ -63,11 +72,13 @@ public class MiniMaxPlayer extends ExampleLegalPlayer {
             }
         }
 
+        List<Move> legalMoves = findLegals(role, state, machine);
+//        Move move = legalMoves.get(0);
         List<Move> oppLegalMoves = findLegals(opponent_role, state, machine);
+//        System.out.println("state: " + state);
         int score = 100;
         for (Move oppMove : oppLegalMoves) {
 
-            // List<Move> action;
             List<Move> action = null;
             if (role == roles.get(0)) {
                 action = Arrays.asList(move, oppMove);
@@ -77,7 +88,13 @@ public class MiniMaxPlayer extends ExampleLegalPlayer {
             }
 
             MachineState newState = this.findNext(action, state, machine);
-            int result = maxScore(role, newState);
+            int result;
+            if (this.memo_table.containsKey(newState)) {
+                result = this.memo_table.get(newState);
+            } else {
+                result = maxScore(role, newState);
+                this.memo_table.put(newState, result);
+            }
             if (result < score) {
                 score = result;
             }
@@ -88,14 +105,30 @@ public class MiniMaxPlayer extends ExampleLegalPlayer {
     private int maxScore(Role role, MachineState state)
             throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException {
         StateMachine machine = getStateMachine();
+        List<Role> roles = machine.getRoles();
+        Role opponent_role = null;
+        for (Role r : roles) {
+            if (!r.equals(role)) {
+                opponent_role = r;
+            }
+        }
         if (this.findTerminalp(state, machine)) {
+//            System.out.println("TERMINAL: " + this.findReward(role, state, machine));
             return this.findReward(role, state, machine);
         }
         List<Move> legalMoves = findLegals(role, state, machine);
+        List<Move> oppLegalMoves = findLegals(opponent_role, state, machine);
+        Move oppMove = oppLegalMoves.get(0);
         int score = 0;
         for (Move move : legalMoves) {
-            // List<Move> action = new ArrayList<Move>(Arrays.asList(move));
-            // int result = minScore(role, this.findNext(action, state, machine));
+//            List<Move> action = null;
+//            if (role == roles.get(0)) {
+//                action = Arrays.asList(move, oppMove);
+//
+//            } else {
+//                action = Arrays.asList(oppMove, move);
+//            }
+//            MachineState newState = this.findNext(action, state, machine);
             int result = minScore(role, move, state);
             if (result > score) {
                 score = result;
