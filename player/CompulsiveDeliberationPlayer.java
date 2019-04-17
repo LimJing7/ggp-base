09240d340c8d56922c9fbfe878bec76ba65a8727
Lim Jing <limjing7@gmail.com>
2019-04-16 20:34:48 -0700

    compulsive deliberation player

---
+++ player/CompulsiveDeliberationPlayer.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.ggp.base.apps.player.Player;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+
+public class CompulsiveDeliberationPlayer extends ExampleLegalPlayer {
+
+	public static void main(String[] args) {
+		Player.initialize(new CompulsiveDeliberationPlayer().getName());
+	}
+
+	@Override
+	public Move play(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException {
+		//Gets our state machine (the same one as returned in getInitialStateMachine)
+		//This State Machine simulates the game we are currently playing.
+		StateMachine machine = getStateMachine();
+
+		//Gets the current state we're in (e.g. move 2 of a game of tic tac toe where X just played in the center)
+		MachineState state = getCurrentState();
+
+		//Gets our role (e.g. X or O in a game of tic tac toe)
+		Role role = getRole();
+
+		//Gets all legal moves for our player in the current state
+		List<Move> legalMoves = findLegals(role, state, machine);
+
+		Move chosenMove = null;
+		int bestScore = 0;
+
+		//Returns the move with best score.
+		for (Move move : legalMoves) {
+			List<Move> action = new ArrayList<Move>(Arrays.asList(move));
+			int score = maxScore(role, this.findNext(action, state, machine));
+			if (score == 100) {
+				chosenMove = move;
+				break;
+			} else {
+				if (score > bestScore) {
+					bestScore = score;
+					chosenMove = move;
+				}
+			}
+		}
+
+		//Logging what decisions your player is making as well as other statistics
+		//is a great way to debug your player and benchmark it against other players.
+		System.out.println("I am playing: " + chosenMove);
+		return chosenMove;
+	}
+
+	/**
+	 * Returns the name of the player.
+	 */
+	@Override
+	public String getName() {
+		return "Compulsive Deliberation Player";
+	}
+
+	protected int maxScore(Role role, MachineState state) throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException {
+		StateMachine machine = getStateMachine();
+		if (this.findTerminalp(state, machine)) {
+			return this.findReward(role, state, machine);
+		}
+		List<Move> legalMoves = findLegals(role, state, machine);
+		int best_score = 0;
+		for (Move move : legalMoves) {
+			List<Move> action = new ArrayList<Move>(Arrays.asList(move));
+			int score = maxScore(role, this.findNext(action, state, machine));
+			if (score > best_score) {
+				best_score = score;
+			}
+		}
+		return best_score;
+	}
+
+
+}
