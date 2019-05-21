74e6f9a10110258b44c9bcffa5965aa97f232855
Lim Jing <limjing7@gmail.com>
2019-05-21 01:55:53 -0700

    added topological sort

--- src/main/java/org/ggp/base/util/statemachine/implementation/propnet/SamplePropNetStateMachine.java
+++ src/main/java/org/ggp/base/util/statemachine/implementation/propnet/SamplePropNetStateMachine.java
@@ -14,6 +14,7 @@ import org.ggp.base.util.gdl.grammar.GdlSentence;
 import org.ggp.base.util.propnet.architecture.Component;
 import org.ggp.base.util.propnet.architecture.PropNet;
 import org.ggp.base.util.propnet.architecture.components.Proposition;
+import org.ggp.base.util.propnet.architecture.components.Transition;
 import org.ggp.base.util.propnet.factory.OptimizingPropNetFactory;
 import org.ggp.base.util.statemachine.MachineState;
 import org.ggp.base.util.statemachine.Move;
@@ -24,10 +25,9 @@ import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
 import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 import org.ggp.base.util.statemachine.implementation.prover.query.ProverQueryBuilder;
 
-
 @SuppressWarnings("unused")
 public class SamplePropNetStateMachine extends StateMachine {
-    /** The underlying proposition network  */
+    /** The underlying proposition network */
     private PropNet propNet;
     /** The topological ordering of the propositions */
     private List<Proposition> ordering;
@@ -36,8 +36,8 @@ public class SamplePropNetStateMachine extends StateMachine {
 
     /**
      * Initializes the PropNetStateMachine. You should compute the topological
-     * ordering here. Additionally you may compute the initial state here, at
-     * your discretion.
+     * ordering here. Additionally you may compute the initial state here, at your
+     * discretion.
      */
     @Override
     public void initialize(List<Gdl> description) {
@@ -51,8 +51,8 @@ public class SamplePropNetStateMachine extends StateMachine {
     }
 
     /**
-     * Computes if the state is terminal. Should return the value
-     * of the terminal proposition for the state.
+     * Computes if the state is terminal. Should return the value of the terminal
+     * proposition for the state.
      */
     @Override
     public boolean isTerminal(MachineState state) {
@@ -61,23 +61,21 @@ public class SamplePropNetStateMachine extends StateMachine {
     }
 
     /**
-     * Computes the goal for a role in the current state.
-     * Should return the value of the goal proposition that
-     * is true for that role. If there is not exactly one goal
-     * proposition true for that role, then you should throw a
+     * Computes the goal for a role in the current state. Should return the value of
+     * the goal proposition that is true for that role. If there is not exactly one
+     * goal proposition true for that role, then you should throw a
      * GoalDefinitionException because the goal is ill-defined.
      */
     @Override
-    public int getGoal(MachineState state, Role role)
-            throws GoalDefinitionException {
+    public int getGoal(MachineState state, Role role) throws GoalDefinitionException {
         // TODO: Compute the goal for role in state.
         return -1;
     }
 
     /**
-     * Returns the initial state. The initial state can be computed
-     * by only setting the truth value of the INIT proposition to true,
-     * and then computing the resulting state.
+     * Returns the initial state. The initial state can be computed by only setting
+     * the truth value of the INIT proposition to true, and then computing the
+     * resulting state.
      */
     @Override
     public MachineState getInitialState() {
@@ -89,8 +87,7 @@ public class SamplePropNetStateMachine extends StateMachine {
      * Computes all possible actions for role.
      */
     @Override
-    public List<Move> findActions(Role role)
-            throws MoveDefinitionException {
+    public List<Move> findActions(Role role) throws MoveDefinitionException {
         // TODO: Compute legal moves.
         return null;
     }
@@ -99,8 +96,7 @@ public class SamplePropNetStateMachine extends StateMachine {
      * Computes the legal moves for role in state.
      */
     @Override
-    public List<Move> getLegalMoves(MachineState state, Role role)
-            throws MoveDefinitionException {
+    public List<Move> getLegalMoves(MachineState state, Role role) throws MoveDefinitionException {
         // TODO: Compute legal moves.
         return null;
     }
@@ -109,28 +105,26 @@ public class SamplePropNetStateMachine extends StateMachine {
      * Computes the next state given state and the list of moves.
      */
     @Override
-    public MachineState getNextState(MachineState state, List<Move> moves)
-            throws TransitionDefinitionException {
+    public MachineState getNextState(MachineState state, List<Move> moves) throws TransitionDefinitionException {
         // TODO: Compute the next state.
         return null;
     }
 
     /**
-     * This should compute the topological ordering of propositions.
-     * Each component is either a proposition, logical gate, or transition.
-     * Logical gates and transitions only have propositions as inputs.
+     * This should compute the topological ordering of propositions. Each component
+     * is either a proposition, logical gate, or transition. Logical gates and
+     * transitions only have propositions as inputs.
      *
-     * The base propositions and input propositions should always be exempt
-     * from this ordering.
+     * The base propositions and input propositions should always be exempt from
+     * this ordering.
      *
-     * The base propositions values are set from the MachineState that
-     * operations are performed on and the input propositions are set from
-     * the Moves that operations are performed on as well (if any).
+     * The base propositions values are set from the MachineState that operations
+     * are performed on and the input propositions are set from the Moves that
+     * operations are performed on as well (if any).
      *
      * @return The order in which the truth values of propositions need to be set.
      */
-    public List<Proposition> getOrdering()
-    {
+    public List<Proposition> getOrdering() {
         // List to contain the topological ordering.
         List<Proposition> order = new LinkedList<Proposition>();
 
@@ -141,6 +135,58 @@ public class SamplePropNetStateMachine extends StateMachine {
         List<Proposition> propositions = new ArrayList<Proposition>(propNet.getPropositions());
 
         // TODO: Compute the topological ordering.
+
+        Set<Component> added = new HashSet<>();
+        Set<Component> boundary = new HashSet<Component>();
+        Set<Component> newBoundary = new HashSet<Component>();
+
+        for (Component component : components) {
+            if (component instanceof Transition) {
+                added.add(component);
+            }
+        }
+
+        // init
+        for (Proposition proposition : propositions) {
+            Set<Component> inputs = proposition.getInputs();
+            int count = 0;
+            for (Component input : inputs) {
+                if (!(input instanceof Transition)) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                boundary.add(proposition);
+                order.add(proposition);
+            }
+        }
+        added.addAll(boundary);
+
+        int steps = 1;
+        while (order.size() != propositions.size() && boundary.size() != 0) {
+            steps++;
+            System.out.println(steps);
+            System.out.println(boundary.size());
+            System.out.println(propositions.size() - order.size());
+            for (Component component : boundary) {
+                Set<Component> outputs = component.getOutputs();
+                for (Component output : outputs) {
+                    Set<Component> inputs = output.getInputs();
+                    if (added.containsAll(inputs)) {
+                        if (!added.contains(output)) {
+                            newBoundary.add(output);
+                            if (output instanceof Proposition) {
+                                order.add((Proposition) output);
+                            }
+                        }
+                    }
+                }
+            }
+            added.addAll(newBoundary);
+            boundary.clear();
+            boundary.addAll(newBoundary);
+            newBoundary.clear();
+        }
 
         return order;
     }
@@ -157,20 +203,19 @@ public class SamplePropNetStateMachine extends StateMachine {
      * The Input propositions are indexed by (does ?player ?action).
      *
      * This translates a list of Moves (backed by a sentence that is simply ?action)
-     * into GdlSentences that can be used to get Propositions from inputPropositions.
-     * and accordingly set their values etc.  This is a naive implementation when coupled with
-     * setting input values, feel free to change this for a more efficient implementation.
+     * into GdlSentences that can be used to get Propositions from
+     * inputPropositions. and accordingly set their values etc. This is a naive
+     * implementation when coupled with setting input values, feel free to change
+     * this for a more efficient implementation.
      *
      * @param moves
      * @return
      */
-    private List<GdlSentence> toDoes(List<Move> moves)
-    {
+    private List<GdlSentence> toDoes(List<Move> moves) {
         List<GdlSentence> doeses = new ArrayList<GdlSentence>(moves.size());
         Map<Role, Integer> roleIndices = getRoleIndices();
 
-        for (int i = 0; i < roles.size(); i++)
-        {
+        for (int i = 0; i < roles.size(); i++) {
             int index = roleIndices.get(roles.get(i));
             doeses.add(ProverQueryBuilder.toDoes(roles.get(i), moves.get(index)));
         }
@@ -179,40 +224,38 @@ public class SamplePropNetStateMachine extends StateMachine {
 
     /**
      * Takes in a Legal Proposition and returns the appropriate corresponding Move
+     *
      * @param p
      * @return a PropNetMove
      */
-    public static Move getMoveFromProposition(Proposition p)
-    {
+    public static Move getMoveFromProposition(Proposition p) {
         return new Move(p.getName().get(1));
     }
 
     /**
      * Helper method for parsing the value of a goal proposition
+     *
      * @param goalProposition
      * @return the integer value of the goal proposition
      */
-    private int getGoalValue(Proposition goalProposition)
-    {
+    private int getGoalValue(Proposition goalProposition) {
         GdlRelation relation = (GdlRelation) goalProposition.getName();
         GdlConstant constant = (GdlConstant) relation.get(1);
         return Integer.parseInt(constant.toString());
     }
 
     /**
-     * A Naive implementation that computes a PropNetMachineState
-     * from the true BasePropositions.  This is correct but slower than more advanced implementations
-     * You need not use this method!
+     * A Naive implementation that computes a PropNetMachineState from the true
+     * BasePropositions. This is correct but slower than more advanced
+     * implementations You need not use this method!
+     *
      * @return PropNetMachineState
      */
-    public MachineState getStateFromBase()
-    {
+    public MachineState getStateFromBase() {
         Set<GdlSentence> contents = new HashSet<GdlSentence>();
-        for (Proposition p : propNet.getBasePropositions().values())
-        {
+        for (Proposition p : propNet.getBasePropositions().values()) {
             p.setValue(p.getSingleInput().getValue());
-            if (p.getValue())
-            {
+            if (p.getValue()) {
                 contents.add(p.getName());
             }
 
